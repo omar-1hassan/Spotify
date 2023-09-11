031c766254c594ec4f2a8d0b2962970d0b4ead8e
omarmohassan24@gmail.com <https://www.github.com/om-del>
2023-09-11 07:42:35 +0300

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by mac on 11/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
